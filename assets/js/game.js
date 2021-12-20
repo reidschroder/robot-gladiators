1cabf3082483cdeda3b6aa30bb9ed947c2e615df
Reid Schroder <reidschroder2@gmail.com>
2021-12-20 16:33:07 -0500

    created initial game

---
+++ assets/js/game.js
@@ -0,0 +1,28 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
+
+var playerName = "smewthe";
+
+// Tony the Robot is ready for battle!
+console.log("smewthe" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
