967b4237beea573c53f8663ddb95d57c7d4dbce9
Reid Schroder <reidschroder2@gmail.com>
2022-01-01 19:02:50 -0500

    bug fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,8 +5,24 @@ var randomNumber = function(min,max) {
   return value;
 }
 
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+// ***************************************
+ while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+// ***************************************
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
+
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -144,6 +160,7 @@ var startGame = function() {
     for(var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            //debugger;
         
             // pick new enemy to fight based on the index of the enemy.names array
             var pickedEnemyObj = enemyInfo[i];
